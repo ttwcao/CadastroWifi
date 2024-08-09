44f95531b68fea09d94d09097c3cb494c825cd3a
Fábio Jr. Griesang <fabio.griesang@outlook.com>
2024-08-09 14:36:39 -0300

    Setuo do projeto inicial

---
+++ cadastro/src/test/java/br/edu/iffarroupilha/ja/bd/cadastro/CadastroApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.iffarroupilha.ja.bd.cadastro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
